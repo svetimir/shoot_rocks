45d9779d88c9345c379c592c3ba2b4f3f85ee072
fluxoid <ifi@yandex.ru>
2018-02-07 21:51:16 +0300

    Version 0.2.6

--- sr_main.py
+++ sr_main.py
@@ -1,8 +1,8 @@
 # SHOOTING ROCKS GAME
 # AUTHOR: fluxoid, ifi@yandex.ru
 # STARTED: 05.07.2017
-# VERSION: 0.1.2
-# FILE LATEST REVISION: 06.07.2017
+# VERSION: 0.2.6
+# LATEST FILE REVISION: 07.02.2018
 
 from tkinter import *
 from tkinter import messagebox
@@ -10,28 +10,48 @@ import random
 import math
 import time
 
-version='0.1.2'
+version='0.2.6'
 authors='fluxoid <ifi@yandex.ru>\njazzard <deathwingstwinks@gmail.com>'
+
+# 0.2.7 - ОЖИДАЕТСЯ
+# - моргание корабля при попадании в него камня
+# - новые экстра-прочные камни (с хитпоинтами количеством 3)
+# -- вероятность спавна 15%
+# -- 5х очки за их уничтожение
+# - уровни сложности EASY MEDIUM HARDCORE
+
+# 0.2.6
+# - ускорение скорости спавна и движения камней со временем
+
+# 0.2.5
+# - кораблю добавлены хитпоинты
+# -- больше не ваншотается
+# - интерфейс с очками и хитпоинтами
+
+# версия 0.2.1
+# - графические камушки
+# - новый корабль
 
 # при выстреле порождается новый объект Bullet
 # пуля движется и существует пока либо не столкнется с камнем
 # либо не уйдет за пределы игрового холста
 
-class Vehicle():
+class Vehicle(object):
     def __init__(self,canvas,color):
         self.canvas=canvas
         self.bs=list()
         self.x=0
         #pause flag
         self.p=False
-        #self.id=canvas.create_polygon(225,450,245,450,235,425,fill=color)
-        self.img=PhotoImage(file='sp.gif')
+        self.img=PhotoImage(file='sp-0.2.1.gif')
         self.id=canvas.create_image(245,425,anchor=NW,image=self.img)
         self.canvas_width=self.canvas.winfo_width()
         self.canvas.bind_all('<KeyPress-space>',self.shoot)
         self.canvas.bind_all('<KeyPress-Left>',self.turn_left)
         self.canvas.bind_all('<KeyPress-Right>',self.turn_right)
         self.canvas.bind_all('<Return>',self.spause)
+        # 0.2.5
+        self.hitpoints=5
 
     def draw(self):
         self.canvas.move(self.id,self.x,0)
@@ -42,10 +62,7 @@ class Vehicle():
             self.x=0
 
     def spause(self,event):
-        if self.p==False:
-            self.p=True
-        else:
-            self.p=False
+        self.p=not self.p
 
     def shoot(self,event):
         pos=self.canvas.coords(self.id)
@@ -57,7 +74,11 @@ class Vehicle():
     def turn_right(self,event):
         self.x=5
 
-class Bullet():
+# функция моргания корабля при попадании в него камня
+    def cblink(self):
+        pass
+
+class Bullet(object):
     def __init__(self,canvas,x,y):
         self.d=7
         self.y=-3
@@ -71,15 +92,18 @@ class Bullet():
     def draw(self):
         self.canvas.move(self.id,0,self.y)
         
-class Rock():
-    def __init__(self,canvas):
+class Rock(object):
+    def __init__(self,canvas,velocity):
+# конструктор версии 0.2.6
         self.canvas=canvas
+        self.img=PhotoImage(file='asteroid40x40.gif')
         c=random.randint(20,480)
-        d=random.randint(10,20)
-        self.id=canvas.create_rectangle(c,0,c+d,d,fill='#eeeeee')
-        self.y=1
+        self.id=canvas.create_image(c,0,anchor=NW,image=self.img)
+# стартовая скорость падения камней 1
+# каждые N камней скорость падения чучуть увеличивается
+        self.y=velocity
         self.canvas_height=self.canvas.winfo_height()
-
+        
     # камень движется строго сверху вниз
     def draw(self):
         self.canvas.move(self.id,0,self.y)
@@ -87,6 +111,25 @@ class Rock():
 class Stats():
     def __init__(self):
         self.pts=0
+
+# 0.2.5
+class InfoHP():
+    def __init__(self,canvas,hitpoints):
+        self.canvas=canvas
+        self.hp=hitpoints
+        self.id=canvas.create_text((20,20),anchor=NW,text='Hits: '+str(hitpoints),fill='#ffffff')
+
+    def update(self,hitpoints):
+        self.canvas.itemconfigure(self.id,text='Hits: '+str(hitpoints))
+
+class InfoScore():
+    def __init__(self,canvas,score):
+        self.canvas=canvas
+        self.sc=score
+        self.id=canvas.create_text((20,40),anchor=NW,text='Score: '+str(score),fill='#ffffff')
+
+    def update(self,score):
+        self.canvas.itemconfigure(self.id,text='Score: '+str(score))
 
 tk=Tk()
 score=StringVar()
@@ -106,12 +149,33 @@ canvas.pack()
 
 v=Vehicle(canvas,'red')
 s=Stats()
+infohp=InfoHP(canvas,v.hitpoints)
+infosc=InfoScore(canvas,s.pts)
 gap=0.01
 c=0
 tol=0.001
 rocks=list()
 state=True
+# стартовый интервал спавна (сложности EASY)
 spawn_interval=1.5
+# каждые next_v камней чуть чуть увеличиваем скорость падения
+# и скорость спавна каждые next_sp камней
+spawn_fact=0.1
+# минимальный интервал спавна
+spawn_min=0.4
+# счетчики камней
+rc=0
+rs=0
+# константа ускорения камней
+next_v=10
+# константа ускорения спавна
+next_sp=20
+# скорость падения
+fall_v=1
+# фактор скорости
+v_fact=0.05
+# максимальная скорость
+max_v=10
 
 canvas.bind_all('<Escape>',lambda e: tk.destroy())
 
@@ -133,13 +197,31 @@ while state:
             i.draw()
     # создаем камень
     if c>=spawn_interval:
-        rocks.append(Rock(canvas))
-        c=0
+        rocks.append(Rock(canvas,fall_v))
+        c=0        
+        rc+=1
+        rs+=1
+        # каждые next_v камней чучуть увеличиваем скорость падения
+        # и скорость спавна каждые next_sp камней
+        if rc==next_v:
+            if fall_v<=max_v:
+                fall_v+=v_fact
+            # debug
+            #print('текущая скорость камней: '+str(fall_v))
+            #print('текущий промежуток спавна: '+str(spawn_interval))
+            # end_debug
+            rc=0
+        if rs==next_sp:
+            if spawn_interval>=spawn_min:
+                spawn_interval-=spawn_fact
+            rs=0
+                
     # защита от переполнения
     if len(rocks)>=500:
         rocks.clear()
     if len(v.bs)>=500:
         v.bs.clear()
+    
     # проверка на столкновение
     for i in v.bs:
         a=i.canvas.coords(i.id)
@@ -150,27 +232,41 @@ while state:
         for j in rocks:
             b=j.canvas.coords(j.id)
             #1. пуля сталкивается с камнем
-            if a[2]>=b[0] and a[0]<=b[2]:
-                if a[3]>=b[1] and a[3]<=b[3]:
-                    # print('boom!')
+            if a[2]>=b[0] and a[0]<=b[0]+40:
+                if a[3]>=b[1] and a[3]<=b[1]+40:
                     canvas.delete(i.id)
                     canvas.delete(j.id)
                     v.bs.remove(i)
                     rocks.remove(j)
                     s.pts+=10
                     score.set(('SCORE: ')+str(s.pts))
+                    # обновляем текст информации об очках
+                    infosc.update(s.pts)
             #B. камень уходит за холст
-            if b[3]>=j.canvas_height:
+            if b[1]>=j.canvas_height:
                 canvas.delete(j.id)
                 rocks.remove(j)
     #2. корабль сталкивается с камнем
     for i in rocks:
         a=v.canvas.coords(v.id)
         b=i.canvas.coords(i.id)
-        if a[0]+40>=b[0] and a[0]<=b[2]:
-            if a[1]>=b[1] and a[1]<=b[3]:
-                state=False
-                state_str.set('GAME OVER')
+        if a[0]+40>=b[0] and a[0]<=b[0]+40:
+            if a[1]>=b[1] and a[1]<=b[1]+40:
+                # столкновение произошло
+                # во первых удаляем камень (так или иначе камень всё)
+                canvas.delete(i.id)
+                rocks.remove(i)
+                # если хиты еще есть - живем и декремент их
+                if v.hitpoints>1:
+                    v.hitpoints-=1
+                    # обновляем текст хитпоинтов
+                    infohp.update(v.hitpoints)
+                    # а тут моргаем кораблем изза столкновения
+                    v.cblink()
+                # если же хитпоинт оставался последний - геймовер
+                else:
+                    state=False
+                    state_str.set('GAME OVER')
                 break
     tk.update_idletasks()
     tk.update()
