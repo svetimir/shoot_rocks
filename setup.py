72f19dc3ad1ea39bdd60b8efaeed9af7e6ade164
svetimir <ifi@yandex.ru>
2018-09-05 15:22:20 +0300

    0.3, minor changes

--- setup.py
+++ setup.py
@@ -10,6 +10,28 @@ os.environ['TCL_LIBRARY'] = r'C:\Program Files (x86)\Python36-32\tcl\tcl8.6'
 os.environ['TK_LIBRARY'] = r'C:\Program Files (x86)\Python36-32\tcl\tk8.6'
 PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))    
 
+shortcut_table = [
+    ("DesktopShortcut",        # Shortcut
+     "DesktopFolder",          # Directory_
+     "Shoot Rocks 0.3 Stars Edition",           # Name
+     "TARGETDIR",              # Component_
+     "[TARGETDIR]sr_main.exe",# Target
+     None,                     # Arguments
+     None,                     # Description
+     None,                     # Hotkey
+     None,                     # Icon
+     None,                     # IconIndex
+     None,                     # ShowCmd
+     'TARGETDIR'               # WkDir
+     )
+    ]
+
+# Now create the table dictionary
+msi_data = {"Shortcut": shortcut_table}
+
+# Change some default MSI options and specify the use of the above defined tables
+bdist_msi_options = {'data': msi_data}
+
 options = {
     'build_exe': {
         'include_files':[
@@ -21,6 +43,7 @@ options = {
             'cosm.gif',
          ],
     },
+    "bdist_msi": bdist_msi_options
 }
 
 setup(options=options, name='ShootRocks',
